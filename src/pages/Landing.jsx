17d9014539fd1e4363480740a3421c5aa1a8f3d3
emmanuelnino <emmanuel98tembo@gmail.com>
2023-07-31 04:10:47 -0700

    image

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -60,7 +60,7 @@ const Landing = () => {
                 width="2.5em"
                 className='justify-center transition transform delay-300 duration-300 group-hover:scale-105 w-full flex group-hover:text-red-600 text-zinc-700'
 
-              >
+              > 
                 <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 011.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 01-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 01-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 010 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 011.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 017.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z" />
               </svg>
               <p className='md:text-sm text-xs transition transform delay-300 duration-300 group-hover:opacity-0 text-zinc-500'>Youtube</p>
@@ -131,7 +131,7 @@ const Landing = () => {
             {/* Replace the 'imgSrcArray' with an array of image URLs */}
             {imgSrcArray.map((imgSrc, index) => (
               <div className='flex justify-center flex-box w-full items-center' key={index}>
-                <img src={imgSrc} alt={`Image ${index + 1}`} />
+                <img className='w-56 h-56' key={index} src={imgSrc} alt={`Image ${index + 1}`} />
               </div>
             ))}
           </div>
