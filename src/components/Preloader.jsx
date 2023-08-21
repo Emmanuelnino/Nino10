01915c6c5e1b50f3b089ece74188a498ed266125
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-21 08:24:19 -0700

    changes

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -3,7 +3,7 @@ const Preloader = () => {
  
   return (
     
-    <motion.div initial={{opacity:1,x:0}} whileInView={{opacity:0,x:"100%"}} transition={{duration:3}}  className='absolute inset-0  bg-green-500 z-[69] '>Preloader</motion.div>
+    <motion.div initial={{x:0}} whileInView={{x:"100%"}} transition={{duration:3,delay:2}}  className='absolute inset-0  bg-green-500 z-[69] '>Preloader</motion.div>
   )
 }
 
