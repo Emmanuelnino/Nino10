389adf32d399d726d173c5e7d5ec638d1ef95ad2
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-31 01:23:41 -0700

    changes

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,15 +1,18 @@
 import { motion } from 'framer-motion';
 const Preloader = () => {
- 
+
   return (
-    
-    <motion.div 
-    initial={{x:0}}
-     whileInView={{x:"100%"}} 
-     transition={{duration:2,delay:1}}  
-     className='font-bold text-5xl absolute inset-0 min-h-screen flex justify-center  items-center bg-green-500 z-[69] '>Hello there
-     </motion.div>
-     
+
+    <motion.div
+      initial={{ x: 0 }}
+      whileInView={{ x: "100%" }}
+      transition={{ duration: 2, delay: 1 }}
+      className='font-bold text-5xl absolute inset-0 min-h-screen flex justify-center items-center bg-green-500 z-[69] animate-pulse'
+    >
+      Hello there
+    </motion.div>
+
+
   )
 }
 
