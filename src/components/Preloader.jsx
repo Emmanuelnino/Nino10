a7174693f514a9bbf68747d6d2f433ead05f8493
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-30 19:26:55 -0700

    change

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -7,8 +7,9 @@ const Preloader = () => {
     initial={{x:0}}
      whileInView={{x:"100%"}} 
      transition={{duration:2,delay:1}}  
-     className='font-bold text-5xl absolute inset-0  bg-green-500 z-[69] '>Hello.
+     className='font-bold text-5xl absolute inset-0 min-h-screen flex justify-center  items-center bg-green-500 z-[69] '>Hello there
      </motion.div>
+     
   )
 }
 
