b2249dfdc1f94b0ca24a6261b296e3dc97fbfd55
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-30 18:59:28 -0700

    change

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -3,7 +3,12 @@ const Preloader = () => {
  
   return (
     
-    <motion.div initial={{x:0}} whileInView={{x:"100%"}} transition={{duration:2,delay:1}}  className='font-bold text-5xl absolute inset-0  bg-green-500 z-[69] '>Hello.</motion.div>
+    <motion.div 
+    initial={{x:0}}
+     whileInView={{x:"100%"}} 
+     transition={{duration:2,delay:1}}  
+     className='font-bold text-5xl absolute inset-0  bg-green-500 z-[69] '>Hello.
+     </motion.div>
   )
 }
 
